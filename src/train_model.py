a89f4848eae23ef27a4fd69ec4341f75dde30342
Hyunwoong Chang <hwchang1201@tamu.edu>
2021-04-16 12:51:50 -0500

    change pybulletgym to pybullet

--- src/train_model.py
+++ src/train_model.py
@@ -2,18 +2,20 @@ import os
 import pdb
 
 import gym
-import pybulletgym
+import pybullet
 from pybullet_envs.deep_mimic.gym_env import HumanoidDeepMimicWalkBulletEnv
+
 
 def train_episode(env):
     s = env.reset()
 
     done = False
-    while True:#while not done:
+    while True:  # while not done:
         s_p, r, done, _ = env.step(env.action_space.sample())
         s = s_p
         env.render()
-        #pdb.set_trace()
+        # pdb.set_trace()
+
 
 def main():
     env = gym.make("HumanoidDeepMimicWalkBulletEnv-v1")
@@ -25,6 +27,7 @@ def main():
         train_episode(env)
 
     env.close()
+
 
 if __name__ == "__main__":
     main()
