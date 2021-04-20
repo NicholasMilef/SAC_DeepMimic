0960922457ef71e1f1d0a8ea3dd3862512abee26
Hyunwoong Chang <hwchang1201@tamu.edu>
2021-04-20 14:45:31 -0500

    runnable SAC for the environment

--- src/Solvers/SAC.py
+++ src/Solvers/SAC.py
@@ -78,8 +78,8 @@ class PolicySAC(nn.Module):
     def evaluate(self, state, epsilon=1e-6):
         mean, log_std = self.forward(state)
         std = log_std.exp()
-        loc = torch.zeros(len(mean))
-        scale = torch.ones(len(mean))
+        #loc = torch.zeros(len(mean))
+        #scale = torch.ones(len(mean))
         #mvn = MultivariateNormal(loc, scale_tril=torch.diag(scale))
         #z = mvn.sample()
         normal = Normal(0, 1)
@@ -108,7 +108,6 @@ class SAC(AbstractSolver):
     def __init__(self, env, options):
         super().__init__(env, options)
 
-        self.env = NormalizedActions(gym.make("Pendulum-v0"))
         self.state_size = (self.env.observation_space.shape[0],)
         self.action_size = len(self.env.action_space.sample())
         self.replay_buffer = deque()
@@ -143,95 +142,75 @@ class SAC(AbstractSolver):
         return policy_fn
 
     def train_episode(self, iteration):
+
+        state = self.env.reset()
+        accum_rewards = 0
+        done = False
         t = 1
-        while t < 40000:
-            state = self.env.reset()
-            accum_rewards = 0
-            done = False
-
-            while not done:
-                action = self.policy(state)
-                next_state, reward, done, _ = self.env.step(action)
-                if len(self.replay_buffer) == self.options['replay_memory_size']:
-                    self.replay_buffer.popleft()
-                self.replay_buffer.append([state, action, reward, next_state, done])
-                if len(self.replay_buffer) < self.options['batch_size']:
-                    state = next_state
-                    continue
-                # sample the batch_index
-                batch_index = np.random.choice(len(self.replay_buffer), self.options['batch_size'], replace=False)
-                states = np.zeros((self.options['batch_size'], self.state_size[0]))
-                actions = np.zeros((self.options['batch_size'], ))
-                rewards = np.zeros((self.options['batch_size'], ))
-                next_states = np.zeros((self.options['batch_size'], self.state_size[0]))
-                not_dones = np.zeros((self.options['batch_size'], ))
-                for i, idx in enumerate(batch_index):
-                    s, a, r, n, d = self.replay_buffer[idx]
-                    states[i] = s
-                    actions[i] = a
-                    rewards[i] = r
-                    next_states[i] = n
-                    not_dones[i] = 1 - 1 * d
-
-                states = torch.FloatTensor(states)
-                next_states = torch.FloatTensor(next_states)
-                actions = torch.FloatTensor(actions).unsqueeze(1)
-                rewards = torch.FloatTensor(rewards).unsqueeze(1)
-                not_dones = torch.FloatTensor(np.float32(not_dones)).unsqueeze(1)
-
-                # Value function update
-                new_action, log_prob, epsilon, mean, log_std = self.actor.evaluate(states)
-                Q_value = torch.min(self.QF1(states, new_action), self.QF2(states, new_action))
-                y = Q_value - self.alpha * log_prob
-                value_loss = nn.MSELoss()(self.VF(states), y.detach())
-
-                self.VF_optimizer.zero_grad()
-                value_loss.backward()
-                self.VF_optimizer.step()
-                # Q functions update
-                target_value_next = self.target_VF(next_states)
-                z = rewards + self.options['gamma'] * not_dones * target_value_next
-
-                QF1_loss = nn.MSELoss()(self.QF1(states, actions), z.detach())
-                QF2_loss = nn.MSELoss()(self.QF2(states, actions), z.detach())
-
-                self.QF1_optimizer.zero_grad()
-                QF1_loss.backward()
-                self.QF1_optimizer.step()
-                self.QF2_optimizer.zero_grad()
-                QF2_loss.backward()
-                self.QF2_optimizer.step()
-
-                # actor update
-                actor_loss = (self.alpha * log_prob - Q_value.detach()).mean()
-                self.actor_optimizer.zero_grad()
-                actor_loss.backward()
-                self.actor_optimizer.step()
-                accum_rewards += reward
+        while not done:
+            action = self.policy(state)
+            next_state, reward, done, _ = self.env.step(action)
+            if len(self.replay_buffer) == self.options['replay_memory_size']:
+                self.replay_buffer.popleft()
+            self.replay_buffer.append([state, action, reward, next_state, done])
+            if len(self.replay_buffer) < self.options['batch_size']:
                 state = next_state
                 t += 1
+                continue
+            # sample the batch_index
+            batch_index = np.random.choice(len(self.replay_buffer), self.options['batch_size'], replace=False)
+            states = np.zeros((self.options['batch_size'], self.state_size[0]))
+            actions = np.zeros((self.options['batch_size'], self.action_size))
+            rewards = np.zeros((self.options['batch_size'], ))
+            next_states = np.zeros((self.options['batch_size'], self.state_size[0]))
+            not_dones = np.zeros((self.options['batch_size'], ))
+            for i, idx in enumerate(batch_index):
+                s, a, r, n, d = self.replay_buffer[idx]
+                states[i] = s
+                actions[i] = a
+                rewards[i] = r
+                next_states[i] = n
+                not_dones[i] = 1 - 1 * d
 
-            print(accum_rewards)
+            states = torch.FloatTensor(states)
+            next_states = torch.FloatTensor(next_states)
+            actions = torch.FloatTensor(actions)
+            rewards = torch.FloatTensor(rewards).unsqueeze(1)
+            not_dones = torch.FloatTensor(np.float32(not_dones)).unsqueeze(1)
+
+            # Value function update
+            new_action, log_prob, epsilon, mean, log_std = self.actor.evaluate(states)
+            Q_value = torch.min(self.QF1(states, new_action), self.QF2(states, new_action))
+            y = Q_value - self.alpha * log_prob
+            value_loss = nn.MSELoss()(self.VF(states), y.detach())
+
+            self.VF_optimizer.zero_grad()
+            value_loss.backward()
+            self.VF_optimizer.step()
+            # Q functions update
+            target_value_next = self.target_VF(next_states)
+            z = rewards + self.options['gamma'] * not_dones * target_value_next
+
+            QF1_loss = nn.MSELoss()(self.QF1(states, actions), z.detach())
+            QF2_loss = nn.MSELoss()(self.QF2(states, actions), z.detach())
+
+            self.QF1_optimizer.zero_grad()
+            QF1_loss.backward()
+            self.QF1_optimizer.step()
+            self.QF2_optimizer.zero_grad()
+            QF2_loss.backward()
+            self.QF2_optimizer.step()
+
+            # actor update
+            actor_loss = (self.alpha * log_prob - Q_value.detach()).mean()
+            self.actor_optimizer.zero_grad()
+            actor_loss.backward()
+            self.actor_optimizer.step()
+
+            accum_rewards += reward
+            state = next_state
+            t += 1
+        print(accum_rewards, t)
 
     def print_name(self):
         return 'SAC'
-
-
-class NormalizedActions(gym.ActionWrapper):
-    def action(self, action):
-        low = self.action_space.low
-        high = self.action_space.high
-
-        action = low + (action + 1.0) * 0.5 * (high - low)
-        action = np.clip(action, low, high)
-
-        return action
-
-    def reverse_action(self, action):
-        low = self.action_space.low
-        high = self.action_space.high
-
-        action = 2 * (action - low) / (high - low) - 1
-        action = np.clip(action, low, high)
-
-        return actions
